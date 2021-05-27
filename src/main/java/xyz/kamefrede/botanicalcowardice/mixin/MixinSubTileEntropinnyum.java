31673378bef604ec23d85727e37c6b142b1c897e
Emanuel Ferraz <kamefrede@gmail.com>
2021-05-28 00:09:12 +0100

    Update MixinSubTileEntropinnyum.java

--- src/main/java/xyz/kamefrede/botanicalcowardice/mixin/MixinSubTileEntropinnyum.java
+++ src/main/java/xyz/kamefrede/botanicalcowardice/mixin/MixinSubTileEntropinnyum.java
@@ -4,8 +4,9 @@ import net.minecraft.entity.Entity;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import vazkii.botania.common.block.subtile.generating.SubTileEntropinnyum;
 
-@Mixin(MixinSubTileEntropinnyum.class)
+@Mixin(SubTileEntropinnyum.class)
 public class MixinSubTileEntropinnyum {
 
 	/**
